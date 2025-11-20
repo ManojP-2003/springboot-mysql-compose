2292687a1c91078c49f729c85ce1a23a8771f7b7
Manoj P <manoj03032003@gmail.com>
2025-11-20 22:00:57 +0530

    done

---
+++ src/main/java/com/main/SpringbootMysqlComposeApplication.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlComposeApplication.class, args);
+	}
+
+}
